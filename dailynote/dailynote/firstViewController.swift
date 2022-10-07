19dd9c60a4fd2de7c15755c6428c9135df4ba627
realisshomyang <realisshoman@kakao.com>
2022-10-08 01:48:44 +0900

    3주차 완료..

---
+++ dailynote/dailynote/firstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dailynote
+//
+//  Created by 조명근 on 2022/10/08.
+//
+
+import UIKit
+
+class firstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
